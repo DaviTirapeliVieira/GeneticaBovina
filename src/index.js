2692339ff7f2bf6b1931b380bdcee5c42af97473
DaviTirapeliVieira <davitirapelivieira@outlook.com>
2025-04-24 23:23:12 -0300

    alteracoes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { HashRouter } from "react-router-dom"; // <-- importa aqui
+import { HashRouter } from "react-router-dom";  // <-- Aqui!
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <HashRouter>  {/* <-- Troque o BrowserRouter pelo HashRouter */}
       <App />
     </HashRouter>
   </React.StrictMode>
